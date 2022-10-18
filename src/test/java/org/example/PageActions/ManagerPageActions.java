e0c2acd714502d98e9edbb39810cb219950f3321
Shekhar Arya <aryashekhar08@gmail.com>
2022-10-18 16:07:50 +0530

    Third commit

--- src/test/java/org/example/PageActions/ManagerPageActions.java
+++ src/test/java/org/example/PageActions/ManagerPageActions.java
@@ -1,5 +1,17 @@
 package org.example.PageActions;
 
+import org.example.PageDefinitions.CommonSteps;
+import org.example.PageElements.ManagerPageElements;
+import org.openqa.selenium.WebDriver;
+
 public class ManagerPageActions {
+   WebDriver driver;
+   ManagerPageElements managerPageElements;
+   public ManagerPageActions(CommonSteps commonSteps) {
+      this.driver = commonSteps.getDriver();
+      this.managerPageElements = new ManagerPageElements(driver);
+   }
+
+
 
 }
